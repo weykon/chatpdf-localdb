7db2ae1ca7dad7a13a04720b584d959b000ff8e7
weykon <635883092@qq.com>
2023-05-23 11:51:27 +0800

    update

---
+++ main.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+from langchain.document_loaders import PyPDFLoader
+from langchain.vectorstores import Chroma
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.document_loaders import UnstructuredPDFLoader
+from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+import sys
+import os
+def parse_args(args):
+    for i, arg in enumerate(args):
+        if arg == '--openai_api_key':
+            return args[i+1]
+
+    return None
+def main():
+    openai_api_key = parse_args(sys.argv)
+    if openai_api_key:
+        os.environ['OPENAI_API_KEY'] = openai_api_key
+    else:
+        print("No API key provided. Exiting.")
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
+
+persist_directory = 'db'
+
+loader = UnstructuredPDFLoader("11.pdf")
+pages = loader.load_and_split()
+embeddings = OpenAIEmbeddings()
+
+# to get local file ?
+db = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
+
+vectordb = db.from_documents(pages,embeddings,persist_directory=persist_directory)
+
+vectordb.persist()
+
+chain = load_qa_chain(OpenAI(temperature=0.6), chain_type="stuff")
+
+def run_qa():
+    while True:
+        query = input()
+        docs = vectordb.as_retriever().get_relevant_documents(query)
+        output = chain.run(input_documents=docs, question=query)
+        print(output)
+try:
+    run_qa()
+except Exception as e:
+    print(f"execute error：{e}")
+    run_qa()
---
+++ readme.md
@@ -0,0 +1,6 @@
+# intro
+a simple less 10 lines code for Chat with pdf 
+calculate embeddings once and save at local db as using question and answser tool
+
+- [ ] set pdf param as input
+- [ ] save file of record conversation
